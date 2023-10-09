7e7d0f27bd6aa7a30ec89ce1e7e1a34844285ad3
Skynet Technologies <developer7@skynettechnologies.com>
2023-10-09 15:51:44 +0530

    Update for free version and key

--- lang/en/local_allinoneaccessibility.php
+++ lang/en/local_allinoneaccessibility.php
@@ -20,14 +20,14 @@
  * @package     local_allinoneaccessibility
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
+$current_domain=isset($_SERVER['HTTP_HOST'])?$_SERVER['HTTP_HOST']:'-';
 $string['pluginname'] = 'All In One Accessibility';
 $string['allinoneaccessibilitydesc'] = 'All In One Accessibility';
 $string['allinoneaccessibility:allow'] = 'Allow all for user roles';
 
 
 $string['aioa-licensekey'] = 'License Key';
-$string['aioa-licensekeydesc'] = 'You are currently using Free version which has limited features. <br />Please <a target="_blank" href="https://www.skynettechnologies.com/all-in-one-accessibility#scroll-here">Upgrade</a> to All in One Accessibility Pro and get full version with 52 plus features.';
+$string['aioa-licensekeydesc'] = 'Please <a href="https://www.skynettechnologies.com/add-ons/cart/?add-to-cart=116&variation_id=117&quantity=1&utm_source='.$current_domain.'&utm_medium=moodle-module&&utm_campaign=purchase-plan">Upgrade</a> to full version of All in One Accessibility Pro.';
 $string['aioa-is_enabled'] = 'Enable';
 $string['aioa-yes'] = 'Yes';
 $string['aioa-no'] = 'No';
