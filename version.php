db9447ae5b82ba8e002d68ee21a5e765f67d1dc4
Skynet Technologies <developer7@skynettechnologies.com>
2024-02-12 18:31:11 +0530

    Update as required for pluging with new release

--- version.php
+++ version.php
@@ -25,6 +25,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_allinoneaccessibility';
 $plugin->release = '1.0.1';
-$plugin->version  = 2023081201;
+$plugin->version  = 2023081202;
 $plugin->requires = 2017051524;
 $plugin->maturity = MATURITY_STABLE;
